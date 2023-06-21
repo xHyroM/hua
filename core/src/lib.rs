4e2a1acd02c14201ce3cdf641d341ea011f473a6
xHyroM <github@xhyrom.dev>
2023-06-21 11:20:23 +0200

    refactor: switch to mlua

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,32 +1,25 @@
-use rlua::{InitFlags, Lua, StdLib};
+use mlua::prelude::*;
+use mlua::Result;
 
 mod modules;
 
-#[derive(Default, Debug)]
+#[derive(Debug)]
 pub struct Hua {
     lua: Lua,
 }
 
 impl Hua {
     pub fn new() -> Self {
-        let lua = unsafe {
-            Lua::unsafe_new_with_flags(
-                StdLib::ALL_NO_DEBUG, // Load all Lua standard libraries apart from debug
-                InitFlags::DEFAULT - InitFlags::REMOVE_LOADLIB, // Allow loading binary libraries
-            )
-        };
+        let lua = Lua::new();
 
         Hua { lua }
     }
 
-    pub fn evaluate(&mut self, code: &str) -> Result<(), rlua::Error> {
-        self.lua.context(|ctx| {
-            ctx.load(code).exec()?;
-            Ok(())
-        })
+    pub fn evaluate(&mut self, code: &str) -> Result<()> {
+        self.lua.load(code).exec()
     }
 
-    pub fn populate(&mut self) -> Result<(), rlua::Error> {
-        self.lua.context(modules::populate)
+    pub fn populate(&mut self) -> Result<()> {
+        modules::populate(self.lua)
     }
 }
--- core/src/modules/dummy.rs
+++ core/src/modules/dummy.rs
@@ -1,8 +1,9 @@
+use mlua::prelude::*;
+use mlua::Result;
+
 use std::env;
 
-use rlua::{Context, Table};
-
-pub fn create<'lua>(ctx: Context<'lua>, loaded: &Table<'lua>) -> Result<(), rlua::Error> {
+pub fn create<'lua>(ctx: Lua, loaded: LuaTable) -> Result<()> {
     let hua_module = ctx.create_table()?;
 
     add_version(ctx, &hua_module)?;
@@ -11,7 +12,7 @@ pub fn create<'lua>(ctx: Context<'lua>, loaded: &Table<'lua>) -> Result<(), rlua
     Ok(())
 }
 
-fn add_version<'lua>(ctx: Context<'lua>, modules: &Table<'lua>) -> Result<(), rlua::Error> {
+fn add_version(ctx: Lua, modules: &LuaTable) -> Result<()> {
     let version = ctx.create_function(|_, ()| {
         let version = env!("CARGO_PKG_VERSION");
         Ok(version.to_string())
